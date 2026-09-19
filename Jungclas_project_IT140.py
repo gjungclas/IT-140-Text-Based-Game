28f206725bde8775b3a1d1a90186feefe4f34970
Grace Jungclas <gracejungclas@gmail.com>
2026-09-19 13:17:40 -0400

    Update to final submission version
    Initial code was only a milestone assignment representing only part of the project. This latest addition is the completed project.

--- Jungclas_project_IT140.py
+++ Jungclas_project_IT140.py
@@ -1,94 +1,170 @@
-#
+# START
+"""
+Stormlight Text Adventure Game
+Author: Grace Jungclas, MA, AT Ret
+Description: A text-based python game inspired by The Stormlight Archive.
 
-# Main function for game play
+Attribution Notice:
+    Characters, settings, and lore from 'The Stormlight Archive' are the
+    intellectual property of Brandon Sanderson and published by Tor Books.
+    This is an unofficial, non-commercial fan project.
+"""
+
+# create empty item inventory
+inventory = []
+# function to display instructions
+def show_instructions():
+    instructions = (
+        "Roshar Text Adventure Game\n"
+        "-----------------------------------------------------------------"
+        "\nOdium is at it again and only Khaladin and his Radiant friends "
+        "can stop him from creating another ever storm. "
+        "One problem: none of the team are in the same country. "
+        "\nKhaladin must travel across Roshar and gather his friends before "
+        "facing Odium to stop the ever storm. "
+        "He must go to the Reshi Isles to get Rysn, "
+        "\nHerdaz to pick up Lopen, Jah Keved to retrieve Shallan, "
+        "Alethkar to bribe Adolin, The Shattered Plains to convince "
+        "Dalinar, Thaylenah to recruit Queen Fen, \nAzir to wrangle Lift, "
+        "and Shinovar to bully Szeth to assemble the team."
+        "\nOnce gathered, they collectively form the Knights Radiant. "
+        "\nHowever, each order of the Knights Radiant are designed to counter"
+        "one of Odium’s powers."
+        "\nWithout all his friends, Khaladin cannot hope to stop Odium…\n"
+
+        "\nTo win the game, you must recruit all 8 of the Knights Radiant "
+        "from the countries of Roshar before you encounter Odium. "
+        "If you encounter Odium first, Roshar is doomed.\n"
+        "Move commands Go [Direction], Recruit [Character], or Quit): "
+    )
+    print(instructions)
+
+
+# function taking direction_from_user and current_room to print and change to new room
+def get_new_country(command1, current_country, countries):
+    current_country = countries[current_country][command1]
+    return current_country
+
+
+def move_options(current_country, countries):
+    print('Available directions: ')
+    available_directions = ', '.join(countries[current_country].keys())
+    print(available_directions)
+
+
+# function using input to check for item and add to inventory
+def recruit_character(characters, current_country):
+    # add to inventory
+    char = characters[current_country]
+    inventory.append(char)
+    # output to user the item/character added to the inventory
+    print(f'You recruited {char} from the Knights Radiant.')
+    # remove character from dict
+    characters[current_country] = None
+
+
+# use function to display current room, item(if present), and inventory
+def show_status(current_country, characters, inventory, countries, move_options):
+    print("\nYou are currently in", current_country)
+    # check if character in current country
+    if characters[current_country] != None:
+        print(f'\nYou can recruit {characters[current_country]} from the Knights Radiant.')
+    else:
+        print("\nThere is no Knight Radiant to recruit in", current_country)
+
+    # check if inventory is full
+    if len(inventory) > 0:
+        print("So far, you have recruited the following Knights Radiant: ", inventory)
+    elif len(inventory) == 0:
+        print("So far, you have not recruited any of the Knights Radiant.")
+
+    move_options(current_country, countries)
+
+
+# Main is responsible for gameplay loop.
 def main():
-    # The dictionary links a country to other countries
-    # Also links characters to country
-    roshar_dict = {
+    # Set dictionary where rooms = countries and items = characters
+    countries = {
         'Start': {'South': 'Shinovar', 'East': 'Reshi'},
-        'Reshi': {'South': 'Azir', 'East': 'Herdaz', 'West': 'Start', 'Character': 'Rysn'},
-        'Herdaz': {'South': 'Jah Keved', 'East': 'Alethkar', 'West': 'Azir', 'Character': 'Lopen'},
-        'Jah Keved': {'North': 'Herdaz', 'South': 'Thaylenah', 'East': 'Alethkar', 'West': 'Azir', 'Character': 'Shallan'},
-        'Alethkar': {'South': 'The Shattered Plains', 'West': 'Jah Keved', 'Character': 'Adolin'},
-        'The Shattered Plains': {'North': 'Alethkar', 'West': 'Thaylenah', 'Character': 'Dalinar'},
-        'Thaylenah': {'North': 'Jah Keved', 'East': 'The Shattered Plains', 'West': 'End country', 'Character': 'Fen'},
-        'Azir': {'North': 'Reshi', 'East': 'Jah Keved', 'West': 'Shinovar', 'Character': 'Lift'},
-        'Shinovar': {'North': 'Start', 'East': 'Azir', 'Character': 'Szeth'},
+        'Reshi': {'South': 'Azir', 'East': 'Herdaz', 'West': 'Start'},
+        'Herdaz': {'South': 'Jah Keved', 'East': 'Alethkar', 'West': 'Azir'},
+        'Jah Keved': {'North': 'Herdaz', 'South': 'Thaylenah', 'East': 'Alethkar', 'West': 'Azir'},
+        'Alethkar': {'South': 'The Shattered Plains', 'West': 'Jah Keved'},
+        'The Shattered Plains': {'North': 'Alethkar', 'West': 'Thaylenah'},
+        'Thaylenah': {'North': 'Jah Keved', 'East': 'The Shattered Plains', 'West': 'End'},
+        'Azir': {'North': 'Reshi', 'East': 'Jah Keved', 'West': 'Shinovar'},
+        'Shinovar': {'North': 'Start', 'East': 'Azir'},
         'End': {'East': 'Thaylenah'},
     }
 
-    # start player in a room
-    cur_country = 'Start'
-    # create character inventory
-    inventory = []
+    characters = {
+        'Start': None,
+        'Reshi': 'Rysn',
+        'Herdaz': 'Lopen',
+        'Jah Keved': 'Shallan',
+        'Alethkar': 'Adolin',
+        'The Shattered Plains': 'Dalinar',
+        'Thaylenah': 'Fen',
+        'Azir': 'Lift',
+        'Shinovar': 'Szeth',
+        'End': 'Odium'
+    }
 
+    # start player in a room
+    current_country = 'Start'
     # show player game instructions
-    menu()
+    show_instructions()
 
     # gameplay loop forever
     while True:
-        # gameplay
-        # call function to display current country, item (if present), and inventory
-        show_status(display_options, display_inventory)
-        # get user input
-        direction_from_user = input('Enter move(Go [Direction], Recruit [Character], or Quit):')
-        # capitalize to match dictionary and split for decision branches
-        direction_from_user = direction_from_user.lower().split()
-
-        print("--------------------------------------------------------------")
-
-        # branch evaluating if game is finished
+        # Evaluate if game is finished
         # evaluate for win
-        if cur_country == 'End':
-            # evaluate if all characters are recruited
-            if len(inventory) == 8:
-                # print winning message
-                print('Congrats! You and the Knights')
-                print('Radiant have successfully defended Roshar from Odium!')
-                break
+        if current_country == 'End' and len(inventory) == 8:
+            # print winning message
+            print('Congrats! You and the Knights Radiant have successfully defended Roshar from Odium!')
+            break
 
-            # evaluate for loss
-            elif len(inventory) != 8:
-                # losing message if all characters not recruited
-                print('Odium wins. Muahahahaha!!')
-                break
+        # evaluate for loss
+        elif current_country == 'End' and len(inventory) != 8:
+            # losing message if all characters not recruited
+            print('Odium wins. You have failed Roshar, but maybe not this project')
+            break
+
         else:
+            # pass show status function
+            show_status(current_country, characters, inventory, countries, move_options)
+            print("--------------------------------------------------------------")
+            # get command from player and standardize format
+            user_input = input('Enter go [direction], recruit[character], or quit: \n')
+            command = user_input.title().split()
+            # move loop
+            if len(command) == 2:
+                # move countries loop
+                # isolate country/charcter from user input
+                move = command[1]
+                # determine if moving countries or recruiting character
+                if command[0] == 'go' or 'Go' and move in countries[current_country].keys():
+                    # pass function using input to change rooms and display if item present
+                    current_country = get_new_country(move, current_country, countries)
+                    continue
 
-            # valid moves
-            # use move index to loop through diction or recruit loops
-            if len(direction_from_user) == 2:
-                if direction_from_user[0] == "go" or "Go":
-                    # set country name for dict to reference
-                    direction = direction_from_user[1].capitalize()
-                    # check if direction is valid
-                    if direction in roshar_dict[cur_country]:
-                        # change current current country to new counrty
-                        cur_country = roshar_dict[cur_country][direction]
-                    # invalid move loop
-                    elif direction not in roshar_dict[cur_country]:
-                        print('You cannot go that way. Try again.')
-            # move loop for character/inventory
-            # separate character name
-                elif direction_from_user[0] == "recruit" or "Recruit":
-                    # set character name for dict to reference
-                    char_name = direction_from_user[1].capitalize()
-                    # check if character vaild
-                    if char_name == roshar_dict.values():
-                        # add to inventory
-                        inventory.append(char_name)
-                        # output to user the character added to the inventory
-                        print(f'You recruited {char_name} from the')
-                        print('Knights Radiant.')
-                        del roshar_dict[cur_country]
-                    # invalid move
-                    elif char_name not in roshar_dict[cur_country]:
-                        print("You cannot recruit that way. Please enter")
-                        print("'recruit [character name]'.")
-            # invalid move loop
+                # recruit character loop
+                elif command[0] == 'recruit' or 'Recruit' and characters[current_country] is not None:
+                    # check if recruit valid
+                    if move == characters[current_country]:
+                        # pass function using input to check for item and add to inventory, if present.
+                        recruit_character(characters, current_country)
+                        continue
+
+            # quit loop
+            elif len(command) == 1 and command == 'quit' or 'Quit':
+                print('Thank you for playing!')
+                break
+
+            # invalid loop
             else:
-                print("Invalid command. Please enter 'Go [Direction],'Recruit [Character name],' or Quit.")
+                print("Invalid command, try again. Please enter 'go [direction], 'Recruit [Character],' or Quit.")
+                continue
 
-
-# call gameplay loop
 if __name__ == '__main__':
     main()
